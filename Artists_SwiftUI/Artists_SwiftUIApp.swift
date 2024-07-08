c1b2d4883a616597fa23acdae330449a56c67223
Sai Charan <saicharan12nov@gmail.com>
2024-07-08 10:17:07 -0400

    Initial Commit

---
+++ Artists_SwiftUI/Artists_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artists_SwiftUIApp.swift
+//  Artists_SwiftUI
+//
+//  Created by Sai Charan Thummalapudi on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Artists_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Artists_SwiftUIUITests/Artists_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Artists_SwiftUIUITestsLaunchTests.swift
+//  Artists_SwiftUIUITests
+//
+//  Created by Sai Charan Thummalapudi on 7/8/24.
+//
+
+import XCTest
+
+class Artists_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
